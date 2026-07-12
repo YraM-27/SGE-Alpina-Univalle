dfc96a20623cfc297cf692ef32ee33456b0cb2ea
YraM-27 <murillo.paula@correounivalle.edu.co>
2026-07-12 00:45:44 -0500

    se agrega el home de la pagina

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('clientes/', include('apps.clientes.urls')),
     path('proveedores/', include('apps.proveedor.urls')),
