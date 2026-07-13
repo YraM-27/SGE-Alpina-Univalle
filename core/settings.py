ddf2f50d5666a785365333ceb4e73bda9543478b
Nicol Serna <nicol.serna@correounivalle.edu.co>
2026-07-13 16:20:50 -0500

    Factura estable

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #mis apps
+        #mis apps
     'apps.clientes',
     'apps.proveedor',
     'apps.producto',
